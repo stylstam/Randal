10e5b17ac648ffc6b57f94b553a6f8250ded768f
Paul Stamatoglou <128477930+stylstam@users.noreply.github.com>
2023-11-17 23:03:50 +0200

    Name changes

--- randal/src/App.js
+++ randal/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello, I am Randal. I create random passwords.
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create password
         </a>
       </header>
+      <body>
+        <div id="root"></div>
+        <script src="path-to-your-bundle.js"></script>
+      </body>
     </div>
   );
 }
